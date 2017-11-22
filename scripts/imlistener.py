807182c99e7bc44bc89307984218460bc4cf537c
Marks Calderon <Marks C>
2017-11-22 12:58:14 -0500

    first commit

---
+++ scripts/imlistener.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import rospy
+#import RPi.GPIO as GPIO
+import numpy as np
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+import time
+from std_msgs.msg import String
+from std_msgs.msg import Int32
+
+p = '0'
+ultra = 16
+pub = rospy.Publisher('datoim', Int32, queue_size=10)
+def callback(data):
+    global pub
+    br = CvBridge()
+    rospy.loginfo('receiving image')
+    dx = br.imgmsg_to_cv2(data)
+
+    cv2.imshow("camera", dx)
+    cv2.waitKey(1)
+
+
+def listener():
+    rospy.init_node('listenerim', anonymous=True)
+    rospy.Subscriber('imager',Image, callback)
+    rospy.spin()
+    cv2.destroyAllWindows()
+if __name__ == '__main__':
+    listener()
+	##talker()
---
+++ scripts/imtalker.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+import time
+def talker():
+    pub = rospy.Publisher('imager', Image, queue_size=10)
+    rospy.init_node('talkerim',anonymous=True)
+    rate = rospy.Rate(5) #2hz
+    capture = cv2.VideoCapture(0)
+    br = CvBridge()
+    while not rospy.is_shutdown():
+        [status, img] = capture.read()
+        if status == True:
+            rospy.loginfo('publish image')
+            pub.publish(br.cv2_to_imgmsg(img))
+            rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
